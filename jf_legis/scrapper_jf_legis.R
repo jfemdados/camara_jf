66be111b8088b29f581cde6b534ba17277e7bbf2
Marcello Filgueiras <53457944+marcellofilgueiras@users.noreply.github.com>
2020-12-23 10:20:24 -0300

    iniciais

---
+++ jf_legis/scrapper_jf_legis.R
@@ -0,0 +1,12 @@
+library(tidyverse)
+library(rvest)
+
+leis_jf_2020 <- read_html("leis_jf_2020.txt")
+
+leis_jf_2020_nodes<- leis_jf_2020%>%
+  html_node('table')%>%
+  html_table()
+
+leis_jf_2020_df <- leis_jf_2020_nodes[3:135,]%>%
+  mutate( X2= str_squish(X2))%>%
+  str_split(X2)
